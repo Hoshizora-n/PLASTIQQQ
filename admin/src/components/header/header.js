115f209d067de54fe7c258ad60488a79ce60aa68
dh.atha <athadhaiffathin17@smk.belajar.id>
2022-02-07 19:51:26 +0700

    First Component with CSS

---
+++ admin/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function header() {
+    return (
+        <div className="header">
+            <h1>Header</h1>
+        </div>
+    );
+}
+
+export default header;

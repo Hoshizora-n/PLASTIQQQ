f30b66d6d0f5bc70785acd3822decae088afdac4
dh.atha <athadhaiffathin17@smk.belajar.id>
2022-02-10 09:03:44 +0700

    change enter key to arrow down key

--- admin/src/components/login/login.jsx
+++ admin/src/components/login/login.jsx
@@ -2,21 +2,53 @@ import React from "react";
 import "./login.css";
 
 function Login() {
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+    }
+
+    const handleUsernameEnter = (e) => {
+        e.preventDefault();
+        if (e.key === 'ArrowDown') {
+            document.getElementById('password').focus();
+            return false
+        }
+    }
+
+    const handlePasswordEnter = (e) => {
+        e.preventDefault()
+        if (e.key === 'ArrowDown') {
+            document.getElementById('submit').focus();
+            return false
+        }
+    }
+
+    const handleClick = (e) => {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        if (username === 'admin' && password === 'admin') {
+            window.location.href = '/dashboard';
+        } else {
+            e.preventDefault();
+            console.log('Wrong username or password');
+        }
+    }
+
     return (
         <div className="login">
-            <form>
+            <form onSubmit={ handleSubmitForm }>
                 <h1>Login</h1>
                 <div className="inputForm">
-                    <div className="usernameInput">
+                    <div className="usernameInput" >
                         <label htmlFor="username">Username : </label>
-                        <input type="text" id="username" name="username" />
+                        <input type="text" id="username" name="username" onKeyUp={ handleUsernameEnter } />
                     </div>
-                    <div className="passwordInput">
+                    <div className="passwordInput" >
                         <label htmlFor="password">Password : </label>
-                        <input type="text" id="password" name="password"></input>
+                        <input type="text" id="password" name="password" onKeyUp={ handlePasswordEnter }></input>
                     </div>
                 </div>
-                <button type="submit">Login</button>
+                <button id="submit" onClick={ handleClick }>Login</button>
             </form>
         </div>
     );

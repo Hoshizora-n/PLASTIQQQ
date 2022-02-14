599a587720a15cba5d48e1a07c762ef5cb47815b
dh.atha <athadhaiffathin17@smk.belajar.id>
2022-02-14 17:10:09 +0700

    just adding some console.log

--- admin/src/components/Sales/sales.js
+++ admin/src/components/Sales/sales.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./sales.css";
 
 function Sales() {
-    return <div>
-        <h1>This is Sales Page</h1>
-    </div>;
+    console.log("hello from sales");
+    return (
+        <div>
+            <h1>This is Sales Page</h1>
+        </div>
+    );
 }
 
 export default Sales;
--- admin/src/components/Sidebar/sidebar.js
+++ admin/src/components/Sidebar/sidebar.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./sidebar.css";
 
-const Sidebar = ({logout}) => {
-  return <aside>
-    <nav>
-      <ul>
-        <Link to={'/'}>Dashboard</Link>
-        <Link to={'/goods'}>Goods</Link>
-        <Link to={'/sales'}>Sales</Link>
-        <Link to={'/users'}>Users</Link>
-      </ul>
-    </nav>
-    <div className='logout'>
-      <button onClick={logout}>Logout</button>
-    </div>
-  </aside>;
-}
+const Sidebar = ({ logout }) => {
+    console.log("hello from sidebar");
+    return (
+        <aside>
+            <nav>
+                <ul>
+                    <Link to={"/"}>Dashboard</Link>
+                    <Link to={"/goods"}>Goods</Link>
+                    <Link to={"/sales"}>Sales</Link>
+                    <Link to={"/users"}>Users</Link>
+                </ul>
+            </nav>
+            <div className="logout">
+                <button onClick={logout}>Logout</button>
+            </div>
+        </aside>
+    );
+};
 
 export default Sidebar;

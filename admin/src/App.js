41241dbab91677ffbc5099843b8b701998f62e9d
dh.atha <athadhaiffathin17@smk.belajar.id>
2022-02-16 10:52:11 +0700

    login completed

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import axios from "axios";
 import Login from "./components/Login/login";
-// import Header from "./components/Header/header";
 import Sidebar from "./components/Sidebar/sidebar";
 import Dashboard from "./components/Dashboard/dashboard";
 import Goods from "./components/Goods/goods";
 import Sales from "./components/Sales/sales";
 import Users from "./components/Users/users";
+
+function GetToken() {
+    if (!localStorage.getItem("token")) return <Login />;
+
+    return TokenValidation();
+}
+
+function TokenValidation() {
+    const [auth, setAuth] = useState();
+    useEffect(() => {
+        axios
+            .post("http://localhost:3100/admin/checkToken", {
+                token: localStorage.getItem("token"),
+            })
+            .then((res) => {
+                setAuth(res.data.message);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    if (auth === "Token Valid") return <App />;
+    else return <Login />;
+}
 
 function App() {
     return (
@@ -16,7 +38,6 @@ function App() {
             <Sidebar />
             <Routes>
                 <Route path="/" element={<Dashboard />} />
-                <Route path="/login" element={<Login />} />
                 <Route path="/goods" element={<Goods />} />
                 <Route path="/sales" element={<Sales />} />
                 <Route path="/users" element={<Users />} />
@@ -25,4 +46,4 @@ function App() {
     );
 }
 
-export default App;
+export default GetToken;
--- admin/src/components/login/login.js
+++ admin/src/components/login/login.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 import axios from "axios";
 
 const Login = () => {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
         const data = {
             username: username,
             password: password,
         };
 
+        console.log(data);
+
         axios
-            .post("http://localhost:3100/admin", data)
+            .post("http://192.168.0.106:3100/admin", data)
             .then((res) => {
                 if (res.data.message === "Login Success") {
                     localStorage.setItem("token", res.data.token);
@@ -51,11 +62,23 @@ const Login = () => {
                 <div className="inputForm">
                     <div className="usernameInput">
                         <label htmlFor="username">Username : </label>
-                        <input type="text" id="username" name="username" onKeyUp={handleUsernameArrowDown} />
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            onChange={handleUsernameChange}
+                            onKeyUp={handleUsernameArrowDown}
+                        />
                     </div>
                     <div className="passwordInput">
                         <label htmlFor="password">Password : </label>
-                        <input type="text" id="password" name="password" onKeyUp={handlePasswordArrowDown}></input>
+                        <input
+                            type="text"
+                            id="password"
+                            name="password"
+                            onChange={handlePasswordChange}
+                            onKeyUp={handlePasswordArrowDown}
+                        ></input>
                     </div>
                 </div>
                 <button type="submit" id="submit">

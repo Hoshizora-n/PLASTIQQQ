5909ff365cfb636fe14d4c4b3f710fe85f52e6b8
dh.atha <athadhaiffathin17@smk.belajar.id>
2022-02-17 18:10:19 +0700

    active menu

--- admin/src/components/Sidebar/sidebar.js
+++ admin/src/components/Sidebar/sidebar.js
@@ -12,32 +12,34 @@ const Sidebar = () => {
         window.location.href = "/";
     };
     return (
-        <aside>
-            <div className="sidebar-header">
-                <Link to="#">PLASTIQQQ.</Link>
-            </div>
-            <nav>
-                <ul>
-                    <Link to={"/dashboard"}>
-                        <MdDashboard /> Dashboard
-                    </Link>
-                    <Link to={"/goods"}>
-                        <FiPackage /> Goods
-                    </Link>
-                    <Link to={"/sales"}>
-                        <IoMdCart /> Sales
-                    </Link>
-                    <Link to={"/users"}>
-                        <FiUsers /> Users
-                    </Link>
-                </ul>
-            </nav>
-            <div className="logout">
-                <button onClick={handleLogout}>
-                    <RiLogoutBoxLine /> Logout
-                </button>
-            </div>
-        </aside>
+        <div className="sidebar">
+            <aside>
+                <div className="sidebar-header">
+                    <Link to="#">PLASTIQQQ.</Link>
+                </div>
+                <nav>
+                    <ul>
+                        <Link to={"/dashboard"} id={window.location.pathname === "/dashboard" ? "active" : ""}>
+                            <MdDashboard /> Dashboard
+                        </Link>
+                        <Link to={"/goods"} id={window.location.pathname === "/goods" ? "active" : ""}>
+                            <FiPackage /> Goods
+                        </Link>
+                        <Link to={"/sales"} id={window.location.pathname === "/sales" ? "active" : ""}>
+                            <IoMdCart /> Sales
+                        </Link>
+                        <Link to={"/users"} id={window.location.pathname === "/users" ? "active" : ""}>
+                            <FiUsers /> Users
+                        </Link>
+                    </ul>
+                </nav>
+                <div className="logout">
+                    <button onClick={handleLogout}>
+                        <RiLogoutBoxLine /> Logout
+                    </button>
+                </div>
+            </aside>
+        </div>
     );
 };
 

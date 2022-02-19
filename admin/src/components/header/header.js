0dae68ea969f97f58536a3fb62b67915d0ff3eb9
dh.atha <athadhaiffathin17@smk.belajar.id>
2022-02-19 15:00:33 +0700

    fixed styling inheader

--- admin/src/components/header/header.js
+++ admin/src/components/header/header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./header.css";
+import { FiUser } from "react-icons/fi";
 
 const Header = (props) => {
     let [isOpen, setIsOpen] = useState(false);
@@ -11,7 +12,10 @@ const Header = (props) => {
 
     return (
         <div className="header">
-            <h1>Hello, {props.username}</h1>
+            <h1> Hello, {props.username}</h1>
+            <div className="user-icon">
+                <FiUser />
+            </div>
             <div className="menu-bar-container">
                 <div className="menu-bar" onClick={menuOpen}>
                     <span className={isOpen ? "open" : ""}></span>
